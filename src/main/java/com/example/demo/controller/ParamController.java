5c2baefca76d639ce2c02ba486e961edb63bc0a6
masaki <masaki@192.168.3.82>
2025-02-17 06:17:03 +0900

    コントローラ変更

--- src/main/java/com/example/demo/controller/ParamController.java
+++ src/main/java/com/example/demo/controller/ParamController.java
@@ -2,29 +2,8 @@ package com.example.demo.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 //↓コントロールと認識させるためのアノテーション
 @Controller
 public class ParamController {
 
-	@GetMapping("/get-param")
-	//リクエストをstringに入れる 引数ぎゃくでもOK 
-	public String getParam(@RequestParam String greeting,
-			
-	//パラメータが統一されていない		
-//	public String getParam(@RequestParam(name="g") String greeting,
-			
-	//送られない場合NULLが渡される
-//	public String getParam(@RequestParam(required=false) String greeting,
-			
-	//送られない時(未設定)が引数になる
-//	public String getParam(@RequestParam(defaultValue="(未設定)") String greeting,
-			Model model) {
-	
-		model.addAttribute("mGreeting", greeting);
-		
-		return "display";
-	}
 }
