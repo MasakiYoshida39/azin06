0da7e7c3f74927e9f3485a7fed5844059d7a5396
masaki <masaki@192.168.3.82>
2025-02-17 06:20:45 +0900

    説明追加

--- src/main/java/com/example/demo/controller/ParamController.java
+++ src/main/java/com/example/demo/controller/ParamController.java
@@ -9,15 +9,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 //↓コントロールと認識させるためのアノテーション
 @Controller
 public class ParamController {
+	//入力画面遷移
 	@GetMapping("/input")
 	public String input() {
 		return "input";
 	}
 	
+	///post-paramのポストからリクエストを受け取る
 	@PostMapping("/post-param")
 	public String postParam(@RequestParam String greeting,
 			Model model) {
-
+		//greetingをmGreetingに格納
 		model.addAttribute("mGreeting", greeting);
 		
 		return "display";
